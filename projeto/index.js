ae1c716f492b74cac454bc43b894c1167e00df63
Fabio Reis <fabiovenicius@yahoo.com.br>
2020-07-17 16:00:21 -0300

    Refatoração de código

--- projeto/index.js
+++ projeto/index.js
@@ -1,19 +1,23 @@
 const fn = require('./funcoes')
 const path = require('path')
+const { removerVazio, separarPor } = require('./funcoes')
 
 const caminho = path.join(__dirname,'..','dados','legendas')
 
-const simbolos = ['.', '?','-','"',
+const simbolos = ['.', '?','-','"',',','(',')',
                     '_','<i>','</i>','\r','[',']'
                 ]
 
 fn.lerDiretorio(caminho)
     .then(arquivos => fn.elementosTerminadosCom(arquivos,'.srt'))
     .then(arquivosSRT=>fn.lerArquivos(arquivosSRT))
-    .then(conteudo=>conteudo.join('\n'))
-    .then(todoConteudo=>todoConteudo.split('\n'))
+    .then(fn.mesclarConteudos)
+    .then(fn.separarPor('\n'))
     .then(linhas => fn.removerVazio(linhas))
     .then(linhaComTexto=>fn.removerSeincluirTexto(linhaComTexto,'-->'))
     .then(linhaComNumero=>fn.removerSeSomenteNumero(linhaComNumero))
     .then(fn.removerCaracteres(simbolos))
+    .then(fn.mesclarConteudos)
+    .then(fn.separarPor(" "))
+    .then(removerVazio)
     .then(console.log)
